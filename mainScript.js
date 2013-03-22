91b9f3084462e262350fe7866441542ffd461051
Dan Becker <github@fullerbecker.com>
2013-03-22 15:38:35 -0700

    Display images from data in the gallery
    - index.html no longer pre-populated with placeholder images; instead,
    gallery starts empty.
    - button to load placeholders from lorempixel
    - button to draw current items
    - button to drop current items and remove their images from gallery

--- mainScript.js
+++ mainScript.js
@@ -2,8 +2,6 @@
  * Created with JetBrains WebStorm.
  * User: dan becker danbecke@uw.edu
  * Date: 3/22/13
- * Time: 12:10 PM
- * To change this template use File | Settings | File Templates.
  */
 
 $(document).ready(function () {
@@ -38,6 +36,7 @@ $(document).ready(function () {
                 );
             }
         });
+        // TODO: automatically display the books after loading the data
     });
 
     // load books from specified Google Books shelf
@@ -45,13 +44,11 @@ $(document).ready(function () {
         // to get all the books from my public TBR shelf, I need to use a few things - hardcoding for now:
         // TODO: clean up hardcoding!
         // TODO: use OAuth 2 instead of API key
-        // TODO: specify all this in an XML manifest; write a general book loader that takes an XML manifest and loads books according to its instruction....?
         // user ID (uid) =  114923424691622086388
         // shelf ID (as_coll) = 1001
-        // API key =  AIzaSyArupT8Pq-t5lN2ke_CVtt6M9PgbfRigzM
+        // API key =  AIzaSyArupT8Pq-t5lN2ke_CVtt6M9PgbfRigzM  TODO: move key from git source to unmanaged config file
         // https://www.googleapis.com/books/v1/users/1112223334445556677/bookshelves/3/volumes?key=yourAPIKey
         // https://www.googleapis.com/books/v1/users/114923424691622086388/bookshelves/1001/volumes?key=AIzaSyArupT8Pq-t5lN2ke_CVtt6M9PgbfRigzM
-
 
         $.getJSON('https://www.googleapis.com/books/v1/users/114923424691622086388/bookshelves/1001/volumes?key=AIzaSyArupT8Pq-t5lN2ke_CVtt6M9PgbfRigzM', function(json) {
             if (json.items) {
@@ -63,6 +60,8 @@ $(document).ready(function () {
                 }
             }
         });
+
+        // TODO: automatically display the books after loading the data
     });
 
     // handy way to clear data without reloading entire page
@@ -77,14 +76,39 @@ $(document).ready(function () {
         myItems.length = 0; // empty the array
     });
 
+    $("#load_lorempixel").click(function () {
+        $('.galleryItem img').attr('src', "http://lorempixel.com/400/600/");
+    });
+
     // output the current myItems to console.log
     $("#console_items").click(function(){
         console.log("Current contents of myItems:");
         for(var i = 0; i < myItems.length; i++ ) {
             console.log("  ", i, "|", myItems[i].itemTitle, "|",myItems[i].imageURL);
         }
+    });
 
+    $("#hide_items").click(function hide_all() {
+        $('.galleryItem img').attr('src', "images/1x1-pixel.png");
+    });
 
+    $("#draw_items").click(function draw_all() {
+        $('.galleryItem img').attr('src', "images/1x1-pixel.png");
+        console.log("draw_all called; old images erased");
+        // load images from myItems into gallery
+        console.log("myItems.length is ", myItems.length);
+        if (myItems.length == 0) {
+            alert("No items to display. Try loading some.");
+        }
+        for(var i = 0; i < myItems.length; i++ ) {
+            var divID = "#g" + i;
+//            var selector = '"' + divID + ' img"' ;
+            console.log(i, divID, myItems[i].imageURL);
+            $(divID).children('img').attr('src', myItems[i].imageURL);
+//            $(selector).attr('src', myItems[i].imageURL);
+//            $(divID).img.attribute("src", myItems[i].imageURL);
+        }
     });
 
 });
+
